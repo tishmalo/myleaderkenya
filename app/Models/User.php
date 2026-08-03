1fef7b536cb28c9470991eee63af09954c2f6fb2
tishmalo <tishmalo99@gmail.com>
2026-08-03 11:23:17 +0300

    fix: import PII encryption trait in user and candidate models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\EncryptsPiiAttributes;
+
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
